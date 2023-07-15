ec7c82130e4e30ca93ed6e0b5f0cd0c131f891e1
NguyenKhanh2101993 <48875493+NguyenKhanh2101993@users.noreply.github.com>
2023-07-15 15:46:46 +0700

    Update paintingChairMain.py
    update

--- QtPaintingChair/paintingChairMain.py
+++ QtPaintingChair/paintingChairMain.py
@@ -888,8 +888,9 @@ class workingWindow:
 
     def stop(self):
         teachWindow.closeTeachWindow()
-        self._threadTeachMode.quit()
-        self._threadTeachMode.wait(100)
+        #self._threadTeachMode.quit()
+        self._threadTeachMode.terminate()
+        self._threadTeachMode.wait()
         self.showStatus("THoat khoi teachmode thread")
 
     def showWorkingWindow(self):
