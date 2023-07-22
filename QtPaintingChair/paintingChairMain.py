9df4e99bac33fbede1089b879b38b8d11f5a0dc0
NguyenKhanh2101993 <48875493+NguyenKhanh2101993@users.noreply.github.com>
2023-07-22 13:51:20 +0700

    update
    update

--- QtPaintingChair/paintingChairMain.py
+++ QtPaintingChair/paintingChairMain.py
@@ -780,7 +780,7 @@ class monitorDatafromArduinoThread(QObject):
                 int, os.popen('free -t -m').readlines()[-1].split()[1:])
  
             # Memory usage
-            ramUsed = round((used_memory/total_memory) * 100, 2)
+            ramUsed = round((used_memory/total_memory) * 100, 1)
             #print("RAM memory % used:", round((used_memory/total_memory) * 100, 2))
 
             getTime = QTime.currentTime()
